99e7193e3db6e909b14b36877806061a57eb73a4
isavalenra <isabella.valenciar@udea.edu.co>
2024-05-09 11:27:59 -0500

    clase para cargar las imagenes

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hola mundo")
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pydicom 
+import os
+
+class Imagenes:
+    def __init__(self):
+        self.__img = []
+
+    def cargar_img(self,ruta):
+        self.__img = cv2.imread(ruta)
+
+    
